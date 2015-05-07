4a1b8e22dabb1dd5edafb49ec987ebc7d6a487f4
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-05-07 14:23:52 +0300

    Remove direct usages of ModuleDescriptor#getPackageFragmentProvider()

--- core/descriptors/src/org/jetbrains/kotlin/descriptors/ModuleDescriptor.kt
+++ core/descriptors/src/org/jetbrains/kotlin/descriptors/ModuleDescriptor.kt
@@ -17,21 +17,12 @@
 package org.jetbrains.kotlin.descriptors
 
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
-import org.jetbrains.kotlin.descriptors.impl.PackageViewDescriptorImpl
-import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.platform.PlatformToKotlinClassMap
 import org.jetbrains.kotlin.resolve.ImportPath
 import org.jetbrains.kotlin.types.TypeSubstitutor
 
 public trait ModuleDescriptor : DeclarationDescriptor, ModuleParameters {
     override fun getContainingDeclaration(): DeclarationDescriptor? = null
-
-    public fun getPackageFragmentProvider(): PackageFragmentProvider
-
-    public fun getPackage(fqName: FqName): PackageViewDescriptor? {
-        val fragments = getPackageFragmentProvider().getPackageFragments(fqName)
-        return if (!fragments.isEmpty()) PackageViewDescriptorImpl(this, fqName, fragments) else null
-    }
 
     public val builtIns: KotlinBuiltIns
 
