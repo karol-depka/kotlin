280a5d34ad7cac4d6c5444fdad665eacbe1df748
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-05-27 15:22:29 +0300

    Added 'FunctionLiteralArgumentResolver'

--- compiler/frontend.java/src/org/jetbrains/kotlin/di/InjectorForReplWithJava.java
+++ compiler/frontend.java/src/org/jetbrains/kotlin/di/InjectorForReplWithJava.java
@@ -70,6 +70,7 @@ import org.jetbrains.kotlin.types.expressions.ValueParameterResolver;
 import org.jetbrains.kotlin.resolve.StatementFilter;
 import org.jetbrains.kotlin.resolve.calls.CallCompleter;
 import org.jetbrains.kotlin.resolve.calls.CandidateResolver;
+import org.jetbrains.kotlin.resolve.calls.FunctionLiteralArgumentResolver;
 import org.jetbrains.kotlin.resolve.calls.tasks.TaskPrioritizer;
 import org.jetbrains.kotlin.psi.JetImportsFactory;
 import org.jetbrains.kotlin.resolve.lazy.LazyDeclarationResolver;
@@ -150,6 +151,7 @@ public class InjectorForReplWithJava {
     private final StatementFilter statementFilter;
     private final CallCompleter callCompleter;
     private final CandidateResolver candidateResolver;
+    private final FunctionLiteralArgumentResolver functionLiteralArgumentResolver;
     private final TaskPrioritizer taskPrioritizer;
     private final JetImportsFactory jetImportsFactory;
     private final LazyDeclarationResolver lazyDeclarationResolver;
@@ -235,7 +237,8 @@ public class InjectorForReplWithJava {
         this.valueParameterResolver = new ValueParameterResolver(kotlinJvmCheckerProvider, expressionTypingServices);
         this.statementFilter = new StatementFilter();
         this.candidateResolver = new CandidateResolver();
-        this.callCompleter = new CallCompleter(argumentTypeResolver, candidateResolver);
+        this.functionLiteralArgumentResolver = new FunctionLiteralArgumentResolver(argumentTypeResolver);
+        this.callCompleter = new CallCompleter(argumentTypeResolver, candidateResolver, functionLiteralArgumentResolver);
         this.taskPrioritizer = new TaskPrioritizer(storageManager);
         this.jetImportsFactory = new JetImportsFactory();
         this.lazyDeclarationResolver = new LazyDeclarationResolver(getModuleContext(), bindingTrace);
@@ -309,6 +312,7 @@ public class InjectorForReplWithJava {
         callResolver.setCallCompleter(callCompleter);
         callResolver.setCandidateResolver(candidateResolver);
         callResolver.setExpressionTypingServices(expressionTypingServices);
+        callResolver.setFunctionLiteralArgumentResolver(functionLiteralArgumentResolver);
         callResolver.setTaskPrioritizer(taskPrioritizer);
         callResolver.setTypeResolver(typeResolver);
 
