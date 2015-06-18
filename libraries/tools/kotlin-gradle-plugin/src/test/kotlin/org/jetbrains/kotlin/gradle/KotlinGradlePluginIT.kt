ff45bd08c9c7522f349640d8c893884ffb0b3cba
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-06-18 15:36:20 +0300

    ++ Add basic kapt tests

--- libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
@@ -79,8 +79,8 @@ class KotlinGradleIT: BaseGradleIT() {
             assertContains(":compileKotlin")
             assertContains(":compileJava")
             assertFileExists("build/tmp/kapt/main/wrappers/annotations.main.txt")
-            assertFileExists("build/classes/main/example/TestClass.class")
             assertFileExists("build/generated/source/kapt/main/TestClassGenerated.java")
+            assertFileExists("build/classes/main/example/TestClass.class")
             assertFileExists("build/classes/main/example/TestClassGenerated.class")
         }
     }
